82173cc2582658c0c529554b1ebfd80013dfcee0
Bibek <justwagle@gmail.com>
2019-08-09 22:57:19 -0500

    generate 350000 as the threshold

--- phylanx/execution_tree/primitives/inlining_threshold.hpp
+++ phylanx/execution_tree/primitives/inlining_threshold.hpp
@@ -1,1 +1,10 @@
-#define THRESHOLD 42454424
+//  Copyright (c) 2019 Bibek Wagle
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+#if !defined(PHYLANX_INLINING_THRESHOLD_HPP)
+#define PHYLANX_INLINING_THRESHOLD_HPP
+
+#define THRESHOLD 350000
+
+#endif
--- src/util/generate_threshold.cpp
+++ src/util/generate_threshold.cpp
@@ -1,17 +1,33 @@
-#include<iostream>
-#include<fstream>
+// Copyright (c) 2019 Bibek Wagle
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <fstream>
+#include <iostream>
 
 int main(int argc, char** argv)
 {
     char* filename = argv[1];
     std::ofstream outfile;
     outfile.open(filename);
-    if (outfile){
-    outfile<<"#define THRESHOLD 42454424";
+    if (outfile)
+    {
+        outfile << "//  Copyright (c) 2019 Bibek Wagle\n"
+                << "//\n"
+                << "//  Distributed under the Boost Software License, "
+                << "Version 1.0. (See accompanying\n"
+                << "//  file LICENSE_1_0.txt or copy "
+                << "at http://www.boost.org/LICENSE_1_0.txt)\n"
+                << "#if !defined(PHYLANX_INLINING_THRESHOLD_HPP)\n"
+                << "#define PHYLANX_INLINING_THRESHOLD_HPP\n"
+                << "\n"
+                << "#define THRESHOLD 350000\n\n"
+                << "#endif\n";
     }
     else
     {
-        std::cout<<"Can not open file";
+        std::cout << "Can not open file";
         return -1;
     }
     outfile.close();
