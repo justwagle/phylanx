d5950a81c4eab4096c1c6d4bdf4a1c72a3108a6b
Bibek <justwagle@gmail.com>
2019-08-10 23:04:47 -0500

    add correct code and fix cmake issues

--- src/util/generate_threshold.cpp
+++ src/util/generate_threshold.cpp
@@ -3,11 +3,45 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include <fstream>
-#include <iostream>
 
-int main(int argc, char** argv)
+#include <hpx/hpx.hpp>
+#include <hpx/hpx_init.hpp>
+
+#include <fstream>
+#include <vector>
+
+///////////////////////////////////////////////////////////////////////////////
+double null_function()
 {
+    return 0.0;
+}
+HPX_PLAIN_ACTION(null_function, null_action)
+///////////////////////////////////////////////////////////////////////////////
+
+int hpx_main(int argc, char** argv)
+{
+    int multiplier = 2000;
+    int interval = 350000;
+
+    {
+        const std::uint64_t count = 1000000;
+        const hpx::id_type here = hpx::find_here();
+        std::vector<hpx::future<double>> futures;
+        futures.reserve(count);
+
+        // start the clock
+        hpx::util::high_resolution_timer walltime;
+        for (std::uint64_t i = 0; i < count; ++i)
+        {
+            futures.push_back(hpx::async<null_action>(here));
+        }
+        hpx::wait_all(futures);
+        const double duration = walltime.elapsed();
+
+        double us = 1e6 * duration / count;
+        interval = us * multiplier;
+    }
+
     char* filename = argv[1];
     std::ofstream outfile;
     outfile.open(filename);
@@ -19,17 +53,23 @@ int main(int argc, char** argv)
                 << "Version 1.0. (See accompanying\n"
                 << "//  file LICENSE_1_0.txt or copy "
                 << "at http://www.boost.org/LICENSE_1_0.txt)\n"
-                << "#if !defined(PHYLANX_INLINING_THRESHOLD_HPP)\n"
-                << "#define PHYLANX_INLINING_THRESHOLD_HPP\n"
+                << "#if !defined(PHYLANX_INLINING_THRESHOLD_HPP_AUTOGEN)\n"
+                << "#define PHYLANX_INLINING_THRESHOLD_HPP_AUTOGEN\n"
                 << "\n"
-                << "#define THRESHOLD 350000\n\n"
+                << "#define THRESHOLD " << interval << "\n\n"
                 << "#endif\n";
     }
     else
     {
         std::cout << "Can not open file";
-        return -1;
+        return hpx::finalize();
     }
+
     outfile.close();
-    return 0;
+    return hpx::finalize();
+}
+
+int main(int argc, char** argv)
+{
+    return hpx::init(argc, argv);
 }
